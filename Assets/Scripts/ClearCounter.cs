320f0b4dd4b0f233c0428022822c2df80f177afe
Kooale325 <muhammad.bilal832017@gmail.com>
2024-06-17 23:29:44 +0500

    scriptableobjectsdone

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -4,9 +4,29 @@ using UnityEngine;
 
 public class ClearCounter : MonoBehaviour {
 
+    [SerializeField] private KitchenObjectSO kitchenObjectSO;
+    [SerializeField] private Transform counterTopPoint;
+    [SerializeField] private ClearCounter secondClearCounter;
+    public bool testing;
+    private KitchenObject kitchenObject;
 
+    private void Update() {
+        if(testing && Input.GetKeyDown(KeyCode.W)) {
+            if (kitchenObject != null) {
+                kitchenObject.SetClearCounter(secondClearCounter);
+                Debug.Log(kitchenObject.GetClearCounter());
+            }
+        }
+    }
     public void Interact() {
-        Debug.Log("Interact!");
+        if (kitchenObject == null) {
+            Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
+            kitchenObjectTransform.localPosition = Vector3.zero;
+
+            kitchenObject = kitchenObjectTransform.GetComponent<KitchenObject>();
+            kitchenObject.SetClearCounter(this);
+
+        }
     }
 
 
